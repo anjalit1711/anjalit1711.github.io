2f53eda35bdccc303d3c01f23d618432705dc98c
Anjali <at9422879@gmail.com>
2026-01-14 13:54:56 +0530

    Implement countChart function in darkmode.js
    Add countChart function to display message length

--- darkmode.js
+++ darkmode.js
@@ -1,3 +1,7 @@
 function toggleDarkMode(){
   document.body.classList.toggle("dark");
 }
+function countChart(){
+  let tex=document.getElementByid("message").value;
+  document.getElementByid("message").innerText=text.length;
+}
